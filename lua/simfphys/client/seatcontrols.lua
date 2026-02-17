c2ce77cf688cfb6395c32ac541790c3dcdf72533
The808Error <121003926+The808Error@users.noreply.github.com>
2026-02-16 19:20:03 -0700

    fix startup error for mouse check

--- lua/simfphys/client/seatcontrols.lua
+++ lua/simfphys/client/seatcontrols.lua
@@ -1,15 +1,16 @@
--- Is our mouse visible?
-local previousState = false 
+hook.Add( "InitPostEntity", "simfphys_MouseCheckerInit", function()
+	local previousState = false
 
-timer.Create( "simfphys_mouseStateCheck", 0.25, 0, function()
-	if not LocalPlayer():InVehicle() then return end
+	timer.Create( "simfphys_mouseStateCheck", 0.25, 0, function()
+		if not LocalPlayer():InVehicle() then return end
 
-	local current = vgui.CursorVisible()
-	if current == previousState then return end
+		local current = vgui.CursorVisible()
+		if current == previousState then return end
 
-	previousState = current
-	
-	net.Start( "simfphys_blockcontrols" )
-	net.WriteBool( current )
-	net.SendToServer()
+		previousState = current
+		
+		net.Start( "simfphys_blockcontrols" )
+		net.WriteBool( current )
+		net.SendToServer()
+	end )
 end )
